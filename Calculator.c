5b40c8a816cc02b9988e38616cb107786d2e3f76
=sankethl27 <sankethlreddy@gmail.com>
2024-11-18 12:11:33 +0530

    calculator 1.1 - fixed few bugs

--- Calculator.c
+++ Calculator.c
@@ -23,12 +23,12 @@ void removeWhiteSpaces(char* exp){
 }
 
 int precedence(char op){
-    if(op == '*' || op == '/') return 1;
-    if(op == '+' || op == '-') return 2;
+    if(op == '*' || op == '/') return 2;
+    if(op == '+' || op == '-') return 1;
     return 0;
 }
 
-int performOperation(int a , int b , char op){
+long long int performOperation(long long int a ,long long int b , char op){
     switch(op){
         case '/': 
             if(b == 0) printError("Error: Division by zero.");
@@ -44,14 +44,16 @@ int performOperation(int a , int b , char op){
 
 long long int calculate(char* exp){
     char operators[MaxLen];
-    int values_stack[MaxLen];
-    int values_top = -1, operators_top = -1;
+    long long int values_stack[MaxLen];
+    long long int values_top = -1, operators_top = -1;
     for(int i = 0; exp[i] != '\0'; i++){
+
+        // if(isspace(exp[i])) continue;
         if(isdigit(exp[i])){
-            int value = 0;
+            long long int value = 0;
 
             while(exp[i] != '\0' && isdigit(exp[i])){
-                value = value * 10 + (exp[i] - '0');
+                value = value * 10 + 1ll*(exp[i] - '0');
                 i++;
             }
             
@@ -71,7 +73,12 @@ long long int calculate(char* exp){
             operators_top--;
         }
         else if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/' ){
-            while(operators_top >= 0 && precedence(exp[i]) < precedence(operators[operators_top])){
+            while(operators_top >= 0 && precedence(exp[i]) <= precedence(operators[operators_top])){
+
+                if (values_top < 1) {
+                    printError("Error: Invalid expression.");
+                }
+                
                 int b = values_stack[values_top--];
                 int a = values_stack[values_top--];
                 char op = operators[operators_top--];
@@ -84,8 +91,14 @@ long long int calculate(char* exp){
         }
 
 
+
     }
     while(operators_top >=0){
+
+        if (values_top < 1) {
+            printError("Error: Invalid expression.");
+        }
+
         if (operators[operators_top] == '(') {
             printError("Error: Mismatched parentheses.");
         }
@@ -95,7 +108,7 @@ long long int calculate(char* exp){
         values_stack[++values_top] = performOperation(a,b,opreator);  
     }
     // printf("Result : %d" , values_stack[values_top]);
-    return 1ll * values_stack[values_top];
+    return values_stack[values_top];
 }
 
 void main(){
@@ -109,18 +122,23 @@ void main(){
         }
         
         inputString[strcspn(inputString, "\n")] = '\0';
+
+        if (strlen(inputString) == 0) {
+            printf("Please enter a valid expression.\n");
+            continue;
+        }
+
         if (strcmp(inputString, "exit") == 0) {
-        printf("Exiting...\n");
-        break;
-}
-        printf("Input Expression: %s\n", inputString);
+            printf("Exiting...\n");
+            break;
+        }   
 
         
         removeWhiteSpaces(inputString);
 
         long long int result = calculate(inputString);
         
-        printf("Result : %lld" ,result);
+        printf("The result of '%s' is: %lld\n", inputString, result);
 
         printf("\n");
 
